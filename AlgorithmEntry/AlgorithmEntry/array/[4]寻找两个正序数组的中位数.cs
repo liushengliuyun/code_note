c3bacd168d519ee0bc28fa1f57e9ccff42854b53
daiyifan <123456>
2022-07-20 10:18:55 +0800

    其它的环境 + 算法要解决的问题的说明

--- AlgorithmEntry/AlgorithmEntry/array/[4]寻找两个正序数组的中位数.cs
+++ AlgorithmEntry/AlgorithmEntry/array/[4]寻找两个正序数组的中位数.cs
@@ -1,12 +1,53 @@
 using System;
 using System.Collections.Generic;
 
+//给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的 中位数 。 
+//
+// 算法的时间复杂度应该为 O(log (m+n)) 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums1 = [1,3], nums2 = [2]
+//输出：2.00000
+//解释：合并数组 = [1,2,3] ，中位数 2
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums1 = [1,2], nums2 = [3,4]
+//输出：2.50000
+//解释：合并数组 = [1,2,3,4] ，中位数 (2 + 3) / 2 = 2.5
+// 
+//
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// nums1.length == m 
+// nums2.length == n 
+// 0 <= m <= 1000 
+// 0 <= n <= 1000 
+// 1 <= m + n <= 2000 
+// -10⁶ <= nums1[i], nums2[i] <= 10⁶ 
+// 
+//
+// Related Topics 数组 二分查找 分治 👍 5647 👎 0
+
+
+//todo 这题测试没有过, 找下原因
 namespace _4
 {
     public class Solution
     {
         //归并排序
-        public double FindMedianSortedArrays1(int[] nums1, int[] nums2)
+        public static double FindMedianSortedArrays1(int[] nums1, int[] nums2)
         {
             List<int> list = new List<int>();
             int i = 0, j = 0;
@@ -43,9 +84,8 @@ namespace _4
             }
         }
 
-
         //不使用list
-        public double FindMedianSortedArrays2(int[] nums1, int[] nums2)
+        public static double FindMedianSortedArrays2(int[] nums1, int[] nums2)
         {
             //取总长度,计算中位数 middle=len/2
             int i = 0, j = 0;
@@ -112,6 +152,11 @@ namespace _4
                 }
             }
 
+            return 0;
+        }
+
+        public static double Test(int[] nums1, int[] nums2)
+        {
             return 0;
         }
     }
