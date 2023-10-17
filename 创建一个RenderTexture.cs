66dee4364e5e84cb5af7f30aaf3d19c14c2c509e
a yi <401021110@qq.com>
2023-10-17 14:17:21 +0800

    笔记

---
+++ 创建一个RenderTexture.cs
@@ -0,0 +1,6 @@
+﻿namespace AlgorithmEntry;
+
+public class 创建一个RenderTexture
+{
+    RenderTexture.GetTemporary(Screen.width, Screen.height, 24, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Default);
+}
