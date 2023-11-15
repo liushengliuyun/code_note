de7654bdcc83ec81350c2430aedd368c76ca12e1
a yi <401021110@qq.com>
2023-11-15 10:28:27 +0800

    使图片透明部分可点穿  MonoBehaviour

---
+++ 使图片透明部分可点穿.cs
@@ -0,0 +1,12 @@
+﻿namespace DefaultNamespace;
+
+
+
+[RequireComponent(typeof(Image))]
+public class 使图片透明部分可点穿: MonoBehaviour
+{
+    private void Awake()
+    {
+        GetComponent<Image>().alphaHitTestMinimumThreshold = 0.5f;
+    }
+}
