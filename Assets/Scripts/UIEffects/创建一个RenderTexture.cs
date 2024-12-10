69afa967542817bf3f8d4da61ae29236ba535c31
代逸凡 <401021110@qq.com>
2024-12-10 11:49:20 +0800

    curlyui

--- Assets/Scripts/UIEffects/创建一个RenderTexture.cs
+++ Assets/Scripts/UIEffects/创建一个RenderTexture.cs
@@ -4,6 +4,7 @@ public static class  创建一个RenderTexture
 {
     public static void CreateRenderTexture()
     {
+        //这个RenderTexture可以保留深度信息和颜色信息
         RenderTexture.GetTemporary(Screen.width, Screen.height, 24, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Default);
     }
     
