11f904c27440a8e38d48dc01b63950de980841c8
代逸凡 <401021110@qq.com>
2024-12-10 17:48:16 +0800

    CUIGraphic graphic 通过控制它的RefCurvesControlRatioPoints , 可以调节曲度

--- Assets/Scripts/UIEffects/创建一个RenderTexture.cs
+++ Assets/Scripts/UIEffects/创建一个RenderTexture.cs
@@ -2,9 +2,10 @@
 
 public static class  创建一个RenderTexture
 {
+    ///这个RenderTexture可以保留深度信息和颜色信息
+    /// CUIGraphic graphic 通过控制它的RefCurvesControlRatioPoints , 可以调节曲度
     public static void CreateRenderTexture()
     {
-        //这个RenderTexture可以保留深度信息和颜色信息
         RenderTexture.GetTemporary(Screen.width, Screen.height, 24, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Default);
     }
     
